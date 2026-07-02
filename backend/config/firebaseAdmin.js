fd28e9a44b416af15ba188167252271a02bea1e0
Swati singh <swatigola274@email.com>
2026-07-02 14:44:12 +0530

    Move .gitignore to project root

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require("firebase-admin/app");
+require("dotenv").config();
 
-const serviceAccount = require("../serviceAccountKey.json");
-
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 initializeApp({
   credential: cert(serviceAccount),
 });
